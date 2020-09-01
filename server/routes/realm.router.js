34dfa6e36e5edc9a814d7a17bb4ab87eef80794b
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-09-01 13:02:09 -0500

    Add Realm Route created
    Co-authored-by: Mase Santos <hola@masesantos.work>

--- server/routes/realm.router.js
+++ server/routes/realm.router.js
@@ -6,7 +6,7 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 /**
  * GET route template
  */
-router.get('/', rejectUnauthenticated, async (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
   console.log('Getting realm for', req.user);
   const queryText = `SELECT * FROM "user"
                       JOIN "project" ON "user"."project_id" = "project"."id"
@@ -19,6 +19,16 @@ router.get('/', rejectUnauthenticated, async (req, res) => {
     .catch(() => res.sendStatus(500));
 });
 
+// Get route for each Realm
+router.get('/:id', rejectUnauthenticated, async (req, res) => {
+  console.log('Getting realm for', req.user);
+  const queryText = `SELECT * FROM "user"`
+  pool.query(queryText, [req.user.id])
+    .then((result) => res.send(result.rows))
+    .catch(() => res.sendStatus(500));
+});
+
+
 // Get route to get each form question page.
 router.get('/form/:id', rejectUnauthenticated, async (req, res) => {
 
@@ -27,7 +37,59 @@ router.get('/form/:id', rejectUnauthenticated, async (req, res) => {
 /**
  * POST route template
  */
-router.post('/', rejectUnauthenticated, (req, res) => {
+// router.post('/', rejectUnauthenticated, (req, res) => {
+
+// });
+
+
+// Route for creating a new Realm
+router.post('/', rejectUnauthenticated, async (req, res) => {
+  const realmName = req.body.realmName;
+  const coverPhotoLink = req.body.coverPhotoLink;
+  const realmDescription = req.body.realmDescription;
+
+  // COMMENT ME OUT ONCE THIS ROUTE WORKS
+  console.log('req.body:', req.body);
+
+  const connection = await pool.connect()
+  try {
+    await connection.query('BEGIN');
+    const addRealmQuery = `INSERT INTO realm (realm_name, description, cover_photo)
+    VALUES ($1, $2, $3) RETURNING id`;
+    // Save the result so we can get the returned value
+    const result = await connection.query( addRealmQuery, [realmName, realmDescription, coverPhotoLink]);
+    // Get the id from the result - will have 1 row with the id
+    const realmId = result.rows[0].id;
+
+    const addsectionQuery = `INSERT INTO section (type)
+    VALUES ($1) RETURNING id`;
+    // Save the result so we can get the returned value
+    const sect = await connection.query( addsectionQuery, [5] ); // 5 means form
+    // Get the id from the result - will have 1 row with the id
+    const sectionId = sect.rows[0].id;
+
+    // Loop through the form questions
+    for ( let i = 0; i < req.body.numberOfQuestions; i++ ) {
+      const addQuestionQuery =
+      `INSERT INTO "section_order" (realm_id, index, section_id, content)
+      SELECT * FROM "section_order"
+      JOIN "section" ON "section_order"."section_id" = "section"."id"
+      JOIN "question" ON "section"."id" = "question"."section_id"
+      VALUES ($1, $2, $3, $4);`;
+      const addQuestionValues = [realmId, i, sectionId, req.body.questions[i] ];
+
+      await connection.query( addQuestionQuery, addQuestionValues );
+    }
+
+    await connection.query('COMMIT');
+    res.sendStatus(200);
+  } catch ( error ) {
+    await connection.query('ROLLBACK');
+    console.log(`Transaction Error - Rolling back new account`, error);
+    res.sendStatus(500);
+  } finally {
+    connection.release()
+  }
 
 });
 

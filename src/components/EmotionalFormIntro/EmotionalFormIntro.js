892b0d4bf6bb3ccca3b2342240a740167269b072
“brunoreyes” <bruno619reyes@gmail.com>
2020-09-05 16:59:30 -0500

    added styling to emotional realm form

--- src/components/EmotionalFormIntro/EmotionalFormIntro.js
+++ src/components/EmotionalFormIntro/EmotionalFormIntro.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styles from '../../themes/realmHomeTheme';
-import {
-  withStyles,
-  Grid,
-  Button,
-  // Paper,
-} from '@material-ui/core';
+import { withStyles, Grid, Button, Paper } from '@material-ui/core';
 
 class EmotionalFormIntro extends Component {
   goBack = () => this.props.history.push('/EmotionalHome');
@@ -15,23 +10,43 @@ class EmotionalFormIntro extends Component {
     const { classes } = this.props;
     return (
       <div>
-        <h3 className={classes.realmTitle}>Realm Form Introduction</h3>
-        <p className={classes.realmDescription}>
-          Please fill out these preliminary questions regarding emotional
-          wellness before beginning the realm course.
-        </p>
-        <p className={classes.realmEsitmatedTimeOfCompletion}>
-          Estimated time to complete:{' '}
-          <b className={classes.bold}>5-8 minutes</b> .
-        </p>
-        <div className={classes.realmButtonContainer}>
-          <Button className={classes.realmButton} onClick={this.start}>
-            Start Form
-          </Button>{' '}
-          <Button className={classes.realmButton} onClick={this.goBack}>
-            Back
-          </Button>
-        </div>
+        <Grid
+          container
+          spacing={5}
+          alignItems="center"
+          justify="center"
+          direction="column"
+        >
+          <Grid
+            // className={classes.leftSideFlex}
+            item
+            xs={12}
+            sm={12}
+            md={6}
+            lg={6}
+          >
+            <h3 className={classes.realmTitle}>Realm Form Introduction</h3>
+            <div className={classes.formDescriptionContainer}>
+              <p className={classes.formDescription}>
+                Please fill out these preliminary questions regarding emotional
+                wellness before beginning the realm course.
+              </p>
+              <p className={classes.estimatedTimeOfCompletion}>
+                Estimated time to complete:{' '}
+                <span className={classes.boldTOC}>5-8 minutes</span>
+              </p>
+            </div>
+          </Grid>{' '}
+          <div className={classes.formButtonContainer}>
+            <Button className={classes.realmButton} onClick={this.start}>
+              Start Form
+            </Button>{' '}
+            <Button className={classes.realmButton} onClick={this.goBack}>
+              Back
+            </Button>
+          </div>
+          {/*end container */}
+        </Grid>
       </div>
     );
   }

2297ec48c1a2007e8c4fced7b84766cad4935b99
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-09-06 23:24:44 -0500

    Basic functionality, TODO saving

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button } from '@material-ui/core';
 import Question from '../Question/Question';
+import styles from '../../themes/realmHomeTheme';
+import {
+  withStyles,
+  Grid,
+  Button,
+  // FormControl,
+  // TextField,
+  // Paper,
+} from '@material-ui/core';
 
 class Section extends Component {
   componentDidMount() {
@@ -13,59 +21,97 @@ class Section extends Component {
     });
   }
 
-  // submitHandler = () => {
-
-  // }
+  saveAndContinue = () => {
+    this.props.history.push('/EmotionalSec2')
+  };
+  saveAndReturn = () => {
+    // this.props.dispatch({type : 'SAVE_SECTION'})
+    this.props.history.push('/EmotionalHome')
+  };
 
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <h1>THIS IS SECTION {this.props.match.params.id}</h1>
-        {this.props.reduxState.section.title !== undefined ? (
-          <h3>{this.props.reduxState.section.title}</h3>
-        ) : (
-          'loading'
-        )}
+        <Grid container spacing={0} alignItems="center" justify="center">
+          <Grid
+            // className={classes.leftSideFlex}
+            item
+            xs={12}
+            sm={12}
+            md={6}
+            lg={6}
+          >
+            {/* <h1>THIS IS SECTION {this.props.match.params.id}</h1> */}
+            {this.props.state.section.title !== undefined ? (
+              <h3 className={classes.sectionTitle}>
+                {this.props.state.section.title}
+              </h3>
+            ) : (
+              'loading'
+            )}
 
-        {this.props.reduxState.section.description !== undefined ? (
-          <p>{this.props.reduxState.section.description}</p>
-        ) : (
-          'loading'
-        )}
+            <Grid>
+              {this.props.state.section.video_link !== undefined ? (
+                <Grid className={classes.sectionVideoContainer}>
+                  <iframe
+                    title={'section video'}
+                    frameborder="0"
+                    className={classes.sectionVideo}
+                    src={this.props.state.section.video_link.replace("/watch?v=","/embed/")}
+                  />
+                </Grid>
+              ) : (
+                'loading'
+              )}
+            </Grid>
 
-        <div>
-          {this.props.reduxState.section !== undefined ? (
-            <iframe
-              width="420"
-              title="section video"
-              height="315"
-              src={this.props.reduxState.section.video_link}
-            ></iframe>
-          ) : (
-            'loading'
-          )}
-        </div>
+            {this.props.state.section.description !== undefined ? (
+              <p>{this.props.state.section.description}</p>
+            ) : (
+              'loading'
+            )}
+{/*
+            {this.props.state.section !== undefined
+              ? JSON.stringify(this.props.state.section)
+              : 'loading'} */}
 
-        {this.props.reduxState.section !== undefined
-          ? JSON.stringify(this.props.reduxState.section)
-          : 'loading'}
-
-        {this.props.reduxState.section.questions !== undefined ? (
-          <Question question={this.props.reduxState.section.questions} />
-        ) : (
-          'loading'
-        )}
-
-        <Button>Submit</Button>
-      </div>
+            {this.props.state.section.questions !== undefined ? (
+              <Grid
+              container
+              direction="column"
+              alignItems="stretch"
+              justify="space-evenly"
+              >
+                {this.props.state.section.questions.map( (q, i) =>
+                <Question question={q} key={i} />)}
+              </Grid>
+              ) : (
+              'loading'
+            )}
+            <Grid className={classes.realmButtonContainer}>
+              <Button
+                className={classes.realmButton}
+                onClick={this.saveAndContinue}
+              >
+                Save & Continue
+              </Button>
+              <Button
+                className={classes.realmButton}
+                onClick={this.saveAndReturn}
+              >
+                Save & Exit
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
     );
   }
 }
 
-const mapStateToProps = (reduxState) => ({
-  user: reduxState.user,
-  reduxState,
+const mapStateToProps = (state) => ({
+  user: state.user,
+  state,
 });
 
 // this allows us to use <App /> in index.js
-export default connect(mapStateToProps)(Section);
+export default withStyles(styles)(connect(mapStateToProps)(Section));

80a88698aef110f25ec3b463ace0653f6dea5277
Tom Hoffmann <64169852+hoffmann1226@users.noreply.github.com>
2020-09-08 15:14:36 -0500

    minor edit

--- src/redux/reducers/allSectionsReducer.js
+++ src/redux/reducers/allSectionsReducer.js
@@ -2,11 +2,11 @@ const allSectionsReducer = (state = [], action) => {
     switch (action.type) {
         case "SET_ALL_SECTIONS":
             return action.payload;
-        case "ADD_SECTION":
+        case "ADD_SECTION_ALLSECTIONS":
             // append the new Section to the state
             state.push(action.payload);
             return state;
-        case "REMOVE_SECTION":
+        case "REMOVE_SECTION_ALLESCTIONS":
             // Remove the section that is the same as the action.payload
             state.splice(state.findIndex( i => i === action.payload ));
             return state;

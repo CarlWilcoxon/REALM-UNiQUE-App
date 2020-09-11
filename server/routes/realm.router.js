fbb8d0c30e995adf2cad95766b531c5f5b7db7ea
Tom Hoffmann <64169852+hoffmann1226@users.noreply.github.com>
2020-09-11 14:56:24 -0500

    first part of transaction works, need to fix second part.

--- server/routes/realm.router.js
+++ server/routes/realm.router.js
@@ -34,87 +34,82 @@ router.get('/form/:id', rejectUnauthenticated, async (req, res) => {
 
 });
 
-/**
- * POST route template
- */
-// router.post('/', rejectUnauthenticated, (req, res) => {
 
 // });
 
+// //OLD DON'T USE
+// router.post('/', rejectUnauthenticated,  async (req, res) => {
+//   const realmName = req.body.realmName;
+//   const coverPhotoLink = req.body.coverPhotoLink;
+//   const realmDescription = req.body.realmDescription;
 
-//OLD DON'T USE
-router.post('/', rejectUnauthenticated,  async (req, res) => {
-  const realmName = req.body.realmName;
-  const coverPhotoLink = req.body.coverPhotoLink;
-  const realmDescription = req.body.realmDescription;
+//   // COMMENT ME OUT ONCE THIS ROUTE WORKS
+//   console.log('req.body:', req.body);
 
-  // COMMENT ME OUT ONCE THIS ROUTE WORKS
-  console.log('req.body:', req.body);
+//   const connection = await pool.connect()
+//   try {
+//     await connection.query('BEGIN');
+//     const addRealmQuery = `INSERT INTO "realm" ("realm_name", "description", "cover_photo")
+//     VALUES ($1, $2, $3) RETURNING "id"`;
+//     // Save the result so we can get the returned value
+//     const result = await connection.query( addRealmQuery, [realmName, realmDescription, coverPhotoLink]);
+//     // Get the id from the result - will have 1 row with the id
+//     const realmId = result.rows[0].id;
 
-  const connection = await pool.connect()
-  try {
-    await connection.query('BEGIN');
-    const addRealmQuery = `INSERT INTO "realm" ("realm_name", "description", "cover_photo")
-    VALUES ($1, $2, $3) RETURNING "id"`;
-    // Save the result so we can get the returned value
-    const result = await connection.query( addRealmQuery, [realmName, realmDescription, coverPhotoLink]);
-    // Get the id from the result - will have 1 row with the id
-    const realmId = result.rows[0].id;
+//     const addsectionQuery = `INSERT INTO "section" ("type")
+//     VALUES ($1) RETURNING "id"`;
+//     // Save the result so we can get the returned value
+//     const sectionResponse = await connection.query( addsectionQuery, [5] ); // 5 means form
+//     // Get the id from the result - will have 1 row with the id
+//     const sectionId = sectionResponse.rows[0].id;
 
-    const addsectionQuery = `INSERT INTO "section" ("type")
-    VALUES ($1) RETURNING "id"`;
-    // Save the result so we can get the returned value
-    const sectionResponse = await connection.query( addsectionQuery, [5] ); // 5 means form
-    // Get the id from the result - will have 1 row with the id
-    const sectionId = sectionResponse.rows[0].id;
+//     // Loop through the form questions
+//     for ( let i = 0; i < req.body.questions.length; i++ ) {
+//       // Insert an entry into section_order to keep track of question order
+//       const addOrderQuery =
+//       `INSERT INTO "section_order" ("realm_id", "index", "section_id")
+//       VALUES ($1, $2, $3);`;
+//       const addOrderValues = [realmId, i, sectionId ];
 
-    // Loop through the form questions
-    for ( let i = 0; i < req.body.questions.length; i++ ) {
-      // Insert an entry into section_order to keep track of question order
-      const addOrderQuery =
-      `INSERT INTO "section_order" ("realm_id", "index", "section_id")
-      VALUES ($1, $2, $3);`;
-      const addOrderValues = [realmId, i, sectionId ];
+//       await connection.query( addOrderQuery, addOrderValues );
 
-      await connection.query( addOrderQuery, addOrderValues );
+//       // Insert question into question db
+//       const addQuestionQuery =
+//       `INSERT INTO "question" ("section_id", "content")
+//       VALUES ($1, $2 ) RETURNING "id";`;
+//       const addQuestionValues = [sectionId, req.body.questions[i].question ]
 
-      // Insert question into question db
-      const addQuestionQuery =
-      `INSERT INTO "question" ("section_id", "content")
-      VALUES ($1, $2 ) RETURNING "id";`;
-      const addQuestionValues = [sectionId, req.body.questions[i].question ]
+//       const questionResponse = await connection.query( addQuestionQuery, addQuestionValues );
+//       const questionId = questionResponse.rows[0].id;
 
-      const questionResponse = await connection.query( addQuestionQuery, addQuestionValues );
-      const questionId = questionResponse.rows[0].id;
+//       // If the question is multiple choice...
+//       // if (req.body.questions[i].questionType === 'multiple_choice') {
+//       //   // ...loop through the array of answers, inserting each into the 'multiple_choice' db
+//       //   for (let j = 0; j < req.body.questions[i].answers.length; j++) {
+//       //     const thisAnswer = req.body.questions[i].answers[j];
+//       //     const choiceQuery =
+//       //     `INSERT INTO "multiple_choice" ("question_id", "content", "correct_answer")
+//       //     VALUES ($1, $2, $3);`;
+//       //     const choiceValues = [questionId, thisAnswer.content, thisAnswer.correct_answer ];
 
-      // If the question is multiple choice...
-      // if (req.body.questions[i].questionType === 'multiple_choice') {
-      //   // ...loop through the array of answers, inserting each into the 'multiple_choice' db
-      //   for (let j = 0; j < req.body.questions[i].answers.length; j++) {
-      //     const thisAnswer = req.body.questions[i].answers[j];
-      //     const choiceQuery =
-      //     `INSERT INTO "multiple_choice" ("question_id", "content", "correct_answer")
-      //     VALUES ($1, $2, $3);`;
-      //     const choiceValues = [questionId, thisAnswer.content, thisAnswer.correct_answer ];
+//       //     await connection.query( choiceQuery, choiceValues );
 
-      //     await connection.query( choiceQuery, choiceValues );
+//       //   }
 
-      //   }
+//       // }
+//     }
 
-      // }
-    }
+//     await connection.query('COMMIT');
+//     res.sendStatus(201);
+//   } catch ( error ) {
+//     await connection.query('ROLLBACK');
+//     console.log(`Transaction Error - Rolling back new account`, error);
+//     res.sendStatus(500);
+//   } finally {
+//     connection.release()
+//   }
 
-    await connection.query('COMMIT');
-    res.sendStatus(201);
-  } catch ( error ) {
-    await connection.query('ROLLBACK');
-    console.log(`Transaction Error - Rolling back new account`, error);
-    res.sendStatus(500);
-  } finally {
-    connection.release()
-  }
-
-});
+// });
 
 //GETTING ALL REALMS FOR "VIEW REALMS" PAGE
 router.get("/all", (req, res) => {
@@ -184,12 +179,12 @@ router.post('/addnewrealm',  async (req, res) => {
     console.log (realmId)
 
 
-  // LOOP THROUGH CHOSEN SECTIONS INTO SECTION ORDER TABLE
-    for ( let i = 0; i < chosenSections.length; i++ ) {
-          const orderSectionQuery = `INSERT INTO "section_order" ("realm_id", "index", "section_id")
-          VALUES ($1, $2, $3);`;
-          await connection.query (orderSectionQuery, [realmId, i, chosenSections[i].id ])
-    };
+  // // LOOP THROUGH CHOSEN SECTIONS INTO SECTION ORDER TABLE
+  //   for ( let i = 0; i < chosenSections.length; i++ ) {
+  //         const orderSectionQuery = `INSERT INTO "section_order" ("realm_id", "index", "section_id")
+  //         VALUES ($1, $2, $3);`;
+  //         await connection.query (orderSectionQuery, [realmId, i, chosenSections[i].id ])
+  //   };
   
     await connection.query('COMMIT');
     res.sendStatus(200)

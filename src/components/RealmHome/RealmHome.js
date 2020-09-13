73cba3dba46c9b3e5ee9dd9cab5acc432005a5ac
brunoreyes <bruno619reyes@gmail.com>
2020-09-12 20:04:02 -0500

    added radio button functionality for realm feedback form

--- src/components/RealmHome/RealmHome.js
+++ src/components/RealmHome/RealmHome.js
@@ -1,110 +1,95 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styles from '../../themes/realmHomeTheme';
-import {
-  withStyles,
-  Grid,
-  Button,
-  Typography,
-} from '@material-ui/core';
+import { withStyles, Grid, Button, Typography } from '@material-ui/core';
 // import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 // import CheckBoxIcon from '@material-ui/icons/CheckBox';
 // import LockIcon from '@material-ui/icons/Lock';
 
-
 class RealmHome extends Component {
-
   componentDidMount() {
     this.props.dispatch({
       type: 'FETCH_REALM',
       payload: {
         realmId: this.props.match.params.realm,
       },
-    })
+    });
     this.props.dispatch({
       type: 'FETCH_PROGRESS',
       payload: {
         realmId: this.props.match.params.realm,
       },
-    })
+    });
   }
 
   goContinue = (event) => {
-
-    console.log( this.props.state )
+    console.log(this.props.state);
     // Use the user's progress to determine where to send them.
-    if (this.props.progress.realm_id !== undefined ) {
-
+    if (this.props.progress.realm_id !== undefined) {
       this.props.history.push(
-        `/section/${
-          this.props.match.params.realm
-        }/${
-          ( ( this.props.realm !== undefined && this.props.progress !== undefined )
-           ?
-          this.props.realm.section[this.props.progress.index].section_id : '' )}`
-      )
-
+        `/section/${this.props.match.params.realm}/${
+          this.props.realm !== undefined && this.props.progress !== undefined
+            ? this.props.realm.section[this.props.progress.index].section_id
+            : ''
+        }`
+      );
     } else {
-
-    // Go to this realm's form
-    // /section/:realm/:section
-    this.props.history.push(
-      `/realm-form-intro/${
-        this.props.match.params.realm
-      }/${
-        ( ( this.props.realm !== undefined )
-         ?
-        this.props.realm.section[0].section_id : '' )}`
-    )}
+      // Go to this realm's form
+      // /section/:realm/:section
+      this.props.history.push(
+        `/realm-form-intro/${this.props.match.params.realm}/${
+          this.props.realm !== undefined
+            ? this.props.realm.section[0].section_id
+            : ''
+        }`
+      );
+    }
   };
 
   goBack = () => this.props.history.push('/home');
 
-
   render() {
-
-    const {
-      classes,
-      realm
-    } = this.props;
+    const { classes, realm } = this.props;
 
     return (
-      <Grid
-      container
-      spacing={0}
-      alignItems="center"
-      justify="center"
-      >
+      <Grid container spacing={0} alignItems="center" justify="center">
         <Grid
           container
           item
           direction="column"
           alignItems="center"
           justify="center"
-
           xs={12}
           sm={12}
           md={6}
           lg={6}
         >
           {/* <h1>THIS IS SECTION {this.props.match.params.id}</h1> */}
-          { realm.realm_name !== undefined ? (
+          {realm.realm_name !== undefined ? (
             <Grid item>
-              <Typography variant="h3" className={classes.realmTitle} gutterBottom>
+              <Typography
+                variant="h3"
+                className={classes.realmTitle}
+                gutterBottom
+              >
                 Realm of {realm.realm_name} Wellness
               </Typography>
             </Grid>
-          ) : '' }
+          ) : (
+            ''
+          )}
 
-          { realm.cover_photo !== undefined ? (
+          {realm.cover_photo !== undefined ? (
             <Grid item className={classes.realmCoverContainer}>
-                <img
+              <img
                 className={classes.realmCover}
                 src={realm.cover_photo}
-                alt={realm.realm_name + " realm logo"}
+                alt={realm.realm_name + ' realm logo'}
               />
             </Grid>
-          ) : '' }
+          ) : (
+            ''
+          )}
 
           {realm.description !== undefined ? (
             <Grid item>
@@ -112,27 +97,23 @@ class RealmHome extends Component {
                 {realm.description}
               </Typography>
             </Grid>
-          ) : 'loading' }
-
+          ) : (
+            'loading'
+          )}
 
           <Grid className={classes.realmButtonContainer}>
+            {realm !== undefined ? (
+              <Button className={classes.realmButton} onClick={this.goContinue}>
+                Continue
+              </Button>
+            ) : (
+              ''
+            )}
 
-            { realm !== undefined ?
-            <Button
-              className={classes.realmButton}
-              onClick={this.goContinue}
-            >
-              Continue
-            </Button> : '' }
-
-            <Button
-              className={classes.realmButton}
-              onClick={this.goBack}
-            >
+            <Button className={classes.realmButton} onClick={this.goBack}>
               Go Back
             </Button>
           </Grid>
-
         </Grid>
       </Grid>
     );
